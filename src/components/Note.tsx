f8e96ee717041372a0144c27ba64647d28b47ab3
WassimWeb <wassimbenmansour73@gmail.com>
2024-09-09 17:00:02 +0200

    add notes

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,10 +1,19 @@
-export default function Note() {
+import { NotesType } from "../notes"
+
+interface NoteProps{
+    notes: NotesType[]
+}
+export default function Note({notes}:NoteProps) {
     return(
         <>
-        <div className="note">
-            <h1>first Note</h1>
-            <p>to make sure to be better</p>
-        </div>
+            {notes.map(note => (
+                <>
+                    <div className="note">
+                        <h1>{note.title}</h1>
+                        <p>{note.content}</p>
+                    </div>
+                </>
+            ))}
         </>
     )
 }
