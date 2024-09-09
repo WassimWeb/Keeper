da3ae43f33bf8da2e75784c3501bbebfa2c2557f
WassimWeb <wassimbenmansour73@gmail.com>
2024-09-09 16:35:44 +0200

    add header footer note

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import Note from "./Note";
+
+export default function App() {
+
+    return (
+        <>
+            <Header />
+            <Note />
+            <Footer />
+        </>
+    )
+
+}
